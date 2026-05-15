65a5571834560cb9e20caa39dc25e204e58559e6
dcmadvogadosam-png <dcmadvogadosam@gmail.com>
2026-05-15 10:49:02 -0400

    Add files via upload

---
+++ Portal_DM_Cloudflare_Supabase_Melhorado/config.js
@@ -0,0 +1,5 @@
+// CONFIGURAÇÃO PÚBLICA DO SUPABASE
+// Preencha com os dados do projeto Supabase do cliente.
+// Supabase > Project Settings > API
+window.DM_SUPABASE_URL = "COLE_AQUI_A_URL_DO_SUPABASE";
+window.DM_SUPABASE_ANON_KEY = "COLE_AQUI_A_CHAVE_ANON_PUBLIC";
