c19ffb14fb6ebd638d327479c6223ced22e92f23
dcmadvogadosam-png <dcmadvogadosam@gmail.com>
2026-05-15 10:44:48 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // CONFIGURAÇÃO PÚBLICA DO SUPABASE
 // Preencha com os dados do projeto Supabase do cliente.
 // Supabase > Project Settings > API
-window.DM_SUPABASE_URL = "https://mixdlureekcbkenbtvbr.supabase.co";
-window.DM_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1peGRsdXJlZWtjYmtlbmJ0dmJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NzQxOTksImV4cCI6MjA5NDM1MDE5OX0.jCrtMoUZPf-nGiRt-UN0ex1X31V5HR_bSVF6vc0S_wI";
+window.DM_SUPABASE_URL = "COLE_AQUI_A_URL_DO_SUPABASE";
+window.DM_SUPABASE_ANON_KEY = "COLE_AQUI_A_CHAVE_ANON_PUBLIC";
